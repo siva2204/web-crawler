2eeb571f624e74bed32988cb1e5e14693917b00d
indreshp135 <indreshp135@gmail.com>
2022-01-09 12:33:19 +0530

    feat: Get data from URL

--- crawler/soup.go
+++ crawler/soup.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"regexp"
 	"log"
 	"net/http"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/jdkato/prose/v2"
+	"github.com/bbalet/stopwords"
 )
+
+var IsLetter = regexp.MustCompile(`^[a-z]+$`).MatchString
 
 func URLScrape(url string) ([]string, error) {
 	// Request the HTML page.
@@ -37,12 +42,58 @@ func URLScrape(url string) ([]string, error) {
 	return urls, nil
 }
 
-// func main(){
-// 	urls, err := URLScrape("https://www.google.com/search?q=golang&tbm=isch")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	for _, url := range urls {
-// 		log.Println(url)
-// 	}
-// }
+func PDataScrape(url string) (
+	[]string, error) {
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// array of url
+	var wordArray []string
+
+
+	// Find the review items
+	// doc.Find("p").Each(func(i int , s *goquery.Selection) {
+		// For each item found, get the text
+		text := doc.Text() // s.Text()
+
+		//Return a string where HTML tags and French stop words has been removed
+		cleanContent := stopwords.CleanString(text, "en", true)
+
+		data, err := prose.NewDocument(cleanContent)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Iterate over the doc's tokens:
+		for _, tok := range data.Tokens() {
+			// log.Println(tok.Text, tok.Tag, tok.Label)
+			if IsLetter(tok.Text) {
+				wordArray = append(wordArray, tok.Text)
+			}
+		}
+	// })
+	return wordArray, nil
+}
+
+func main(){
+	urls, err := PDataScrape("http://localhost:5000/compellingly-embrace-from-generation-x-is")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, url := range urls {
+		log.Println(url)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/siva2204/web-crawler
 
 go 1.16
 
-require github.com/PuerkitoBio/goquery v1.8.0 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/bbalet/stopwords v1.0.0 // indirect
+	github.com/jdkato/prose/v2 v2.0.0 // indirect
+)
